39bba0d94bc24eebd22cbf51ac3f3196223ad65a
Zhenis Pir <yenispiro@gmail.com>
2024-08-12 20:35:50 +0500

    Revert "Working on an about-me part of the website."
    This reverts commit 2e158ac56562b77eec47c4ee52e1fd676869c05f.

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,142 +1,55 @@
 <script>
-    import { onMount } from 'svelte';
-    import Main from '$lib/components/Main.svelte';
-
-    let isSubmitted = false;
-    let showError = false;
-
-    let formData = { // all below are string
-        from_page: '',
-        name: '',
-        email: '',
-        message: ''
-    };
-
-    onMount(() => {
-        const params = new URLSearchParams(window.location.search);
-        formData = { ...formData, from_page: params.get('from_page') };
-    });
-
-    const encode = (data) => {
-        return Object.keys(data)
-            .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
-            .join('&');
-    };
-
-    const handleSubmit = (e) => {
-        const { name, email, message, from_page } = formData;
-
-        if (!name || !email || !message) {
-            showError = true;
-            return;
-        }
-
-        const target = e.target;
-        fetch('/', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-            body: encode({
-                'form-name': target.getAttribute('name'),
-                ...formData
-            })
-        })
-            .then(() => {
-                isSubmitted = true;
-            })
-            .catch((error) => alert(error));
-    };
+	import Callout from '$lib/components/Callout.svelte'
 </script>
 
 
 <svelte:head>
-	<title>Zhenis Pir | Contact</title>
-	<meta
-		data-key="description"
-		name="description"
-		content="If you fill out this form, I'll probably send you an email. What a bargain!"
-	/>
+	<title>Contact</title>
 </svelte:head>
 
 
-<Main>
-	<div class="compressed-content">
-		<h2>Get in touch</h2>
+<h1>Contact</h1>
 
-		{#if !isSubmitted}
-			<noscript>
-				<h3>Sorry, this contact form won't work without JavaScript enabled.</h3>
-				<p>
-					I don't do any tracking or anything like that though (except some <a
-						href="https://plausible.io">privacy-respecting analytics</a
-					>), if that's your concern.
-				</p>
-			</noscript>
+This starter was made by Josh Collinsworth. You can <a rel="external" href="https://joshcollinsworth.com/contact">get in touch with Josh here</a>.
 
-			<p class="big">Happy to chat! Email for business related messages.</p>
+If you're using this starter for your own site, feel free to delete this page, or replace it with a contact page of your own. (I'm a big fan of <a href="https://docs.netlify.com/forms/setup/">Netlify forms</a>, personally.)
 
-			<ul class="small">
-				<li>
-					<strong>Yes</strong>, I will work with you if you know the problem and willing to pay.
-				</li>
+<Callout>This form does nothing! It's just here to show default styling.</Callout>
 
-				<li><strong>No</strong>, I do not work for free. Please, do not ask!</li>
-			</ul>
-
-			<form
-				id="contact-form"
-				name="contact"
-				method="post"
-				on:submit|preventDefault={handleSubmit}
-				action="/success/"
-			>
-				<input type="hidden" name="form-name" value="contact" />
-				<p hidden>
-					<label>
-						Don't fill this out: <input name="bot-field" />
-					</label>
-				</p>
-				<input type="hidden" name="from_page" bind:value={formData.from_page} />
-				<div class="sender-info">
-					<div class="field">
-						<label for="name" class="label">Your name:</label>
-						<input
-							type="text"
-							name="name"
-							bind:value={formData.name}
-							placeholder="What should I call you?"
-						/>
-					</div>
-					<div class="field">
-						<label for="email"> Your email: </label>
-						<input
-							type="email"
-							name="email"
-							bind:value={formData.email}
-							placeholder="Where can I send a response?"
-						/>
-					</div>
-				</div>
-
-				<div class="message-wrapper field">
-					<label for="message">Message:</label>
-					<textarea
-						name="message"
-						bind:value={formData.message}
-						rows="6"
-						placeholder="What would you like to talk about?"
-					/>
-				</div>
-
-				{#if showError}
-					<div class="error">Please be sure all above fields are filled out. Thanks!</div>
-				{/if}
-
-				<button type="submit"> Send </button>
-			</form>
-		{:else}
-			<h3>Thanks for your message!</h3>
-
-			<p>I'll follow up by email if this wasn't spam. :)</p>
-		{/if}
+<form on:submit|preventDefault>
+	<div class="form-section">
+		<label for="name">Name</label>
+		<input type="text" id="name" placeholder="First name" />
 	</div>
-</Main>
+	
+	<div class="form-section">
+		<label for="email">Email</label>
+		<input type="email" id="email" placeholder="Email address" />
+	</div>
+
+	<fieldset>
+		<legend>
+			Which option?
+		</legend>
+
+		<div>
+			<input type="radio" name="s" id="s1" value="s1">
+			<label for="s1">Option 1</label>
+		</div>
+		<div>
+			<input type="radio" name="s" id="s2" value="s2">
+			<label for="s2">Option 2</label>
+		</div>
+		<div>
+			<input type="radio" name="s" id="s3" value="s3">
+			<label for="s3">Option 3</label>
+		</div>
+	</fieldset>
+
+	<div class="form-section">
+		<input type="checkbox" id="c1" >
+		<label for="c1">Sign me up for something!</label>
+	</div>
+
+	<input type="submit" value="Do nothing!">
+</form>
